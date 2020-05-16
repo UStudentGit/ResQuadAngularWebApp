0b5aa3364e9718c405895885ddc78976518ce07d
dezet98 <danielzaczek124@gmail.com>
2020-05-16 18:05:33 +0200

    SignUp - link with api

--- src/app/_services/authorization.service.ts
+++ src/app/_services/authorization.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,13 @@ export class AuthorizationService {
     return localStorage.getItem('token');
   }
 
-  login(email: string, password: string) {
-    this.http.post(this.apiUrl, { Email: email, Password: password });
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(this.apiUrl + '/login', { Email: email, Password: password });
+  }
+
+  register(Email: string, Name: string, Surname: string, Password: string): Observable<any> {
+    return this.http.post(this.apiUrl + '/register', { email: Email, name: Name, surname: Surname, password: Password });
   }
 
 }
+
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthorizationService } from '../_services/authorization.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private auth: AuthorizationService, private router: Router) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: ['', Validators.required],
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+  }
+
+  onRegister(): void {
+    this.loading = true;
+    const registerData = this.registerForm.value;
+    this.auth.register(registerData.email, registerData.name, registerData.surname, registerData.password).subscribe((res: any) => {
+      this.router.navigate(['/logIn']);
+      alert(res.message);
+      this.loading = false;
+    }, error => {
+      alert(error.error.message);
+      console.log('Error when login. Error:');
+      console.log(error);
+      this.loading = false;
+    });
   }
 
 }
