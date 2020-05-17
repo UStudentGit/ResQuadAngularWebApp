e471137fa7ab9d7cc03438a6f53c1a5b13077d0e
Tania Zhvanko <tetianazhvanko@gmail.com>
2020-05-17 20:25:28 +0200

    create-event: scss +html + new component

---
+++ src/app/main-panel/create-event/table-events/table-events/table-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-events',
+  templateUrl: './table-events.component.html',
+  styleUrls: ['./table-events.component.scss']
+})
+export class TableEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
